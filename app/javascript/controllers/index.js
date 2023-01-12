190adbff1513ccc63b5244c1501a081cd6c3b2ad
Gordana Marjanovic <gogamar@gmail.com>
2023-01-12 13:17:58 +0100

    apply price filter

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,22 +2,19 @@
 // Run that command whenever you add a new controller or create them with
 // ./bin/rails generate stimulus controllerName
 
-import { application } from "./application"
+import { application } from "./application";
 
-import AddressAutocompleteController from "./address_autocomplete_controller"
-application.register("address-autocomplete", AddressAutocompleteController)
+import AddressAutocompleteController from "./address_autocomplete_controller";
+application.register("address-autocomplete", AddressAutocompleteController);
 
-import FormSubmitController from "./form_submit_controller"
-application.register("form-submit", FormSubmitController)
+import HelloController from "./hello_controller";
+application.register("hello", HelloController);
 
-import HelloController from "./hello_controller"
-application.register("hello", HelloController)
+import LightboxController from "./lightbox_controller";
+application.register("lightbox", LightboxController);
 
-import LightboxController from "./lightbox_controller"
-application.register("lightbox", LightboxController)
+import MapController from "./map_controller";
+application.register("map", MapController);
 
-import MapController from "./map_controller"
-application.register("map", MapController)
-
-import StarRatingController from "./star_rating_controller"
-application.register("star-rating", StarRatingController)
+import StarRatingController from "./star_rating_controller";
+application.register("star-rating", StarRatingController);
