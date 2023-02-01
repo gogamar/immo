b315920fb0d883e58b5d9aa16b681011b2f9b207
Gordana Marjanovic <gogamar@gmail.com>
2023-02-01 01:22:00 +0100

    fixing frontend

--- app/javascript/controllers/typed_js_controller.js
+++ app/javascript/controllers/typed_js_controller.js
@@ -5,8 +5,6 @@ export default class extends Controller {
   static values = { typedtext: Array }
 
   connect() {
-    console.log("Typed.")
-    console.log(this.typedtextValue)
     new Typed(this.element, {
       strings: this.typedtextValue,
       loop: true,
--- app/javascript/packs/custom.js
+++ app/javascript/packs/custom.js
@@ -1,353 +1,6 @@
 $(function () {
   "use strict";
 
-  $(window).on("load", function () {
-    $("#preloader").delay(350).fadeOut("slow");
-    $("body").delay(350).css({ overflow: "visible" });
-  });
-
-  /*---- Bottom To Top Scroll Script ---*/
-  $(window).on("scroll", function () {
-    var height = $(window).scrollTop();
-    if (height > 100) {
-      $("#back2Top").fadeIn();
-    } else {
-      $("#back2Top").fadeOut();
-    }
-  });
-
-  $("#back2Top").on("click", function (event) {
-    event.preventDefault();
-    $("html, body").animate({ scrollTop: 0 }, "slow");
-    return false;
-  });
-
-
-  // Script For Select Adult & Child Number
-  $(function () {
-    var guestAmount = $("#guestNo");
-
-    $("#cnt-up").on("click", function () {
-      guestAmount.val(Math.min(parseInt($("#guestNo").val()) + 1, 20));
-    });
-    $("#cnt-down").on("click", function () {
-      guestAmount.val(Math.max(parseInt($("#guestNo").val()) - 1, 1));
-    });
-  });
-
-  $(function () {
-    var guestAmount = $("#kidsNo");
-
-    $("#kcnt-up").on("click", function () {
-      guestAmount.val(Math.min(parseInt($("#kidsNo").val()) + 1, 20));
-    });
-    $("#kcnt-down").on("click", function () {
-      guestAmount.val(Math.max(parseInt($("#kidsNo").val()) - 1, 0));
-    });
-  });
-
-  // // Check In & Check Out Daterange Script
-  // $(function () {
-  //   $('input[name="checkout"]').daterangepicker({
-  //     singleDatePicker: true,
-  //   });
-  //   $('input[name="checkout"]').val("");
-  //   $('input[name="checkout"]').attr("placeholder", "Check Out");
-  // });
-  // $(function () {
-  //   $('input[name="checkin"]').daterangepicker({
-  //     singleDatePicker: true,
-  //   });
-  //   $('input[name="checkin"]').val("");
-  //   $('input[name="checkin"]').attr("placeholder", "Check In");
-  // });
-
-  // Compare Slide
-  $(".csm-trigger").on("click", function () {
-    $(".compare-slide-menu").toggleClass("active");
-  });
-  $(".compare-button").on("click", function () {
-    $(".compare-slide-menu").addClass("active");
-  });
-
-  // // smart-textimonials
-  // $("#smart-textimonials").slick({
-  //   slidesToShow: 3,
-  //   infinite: true,
-  //   arrows: false,
-  //   autoplay: true,
-  //   responsive: [
-  //     {
-  //       breakpoint: 768,
-  //       settings: {
-  //         arrows: false,
-  //         slidesToShow: 2,
-  //       },
-  //     },
-  //     {
-  //       breakpoint: 480,
-  //       settings: {
-  //         arrows: false,
-  //         slidesToShow: 1,
-  //       },
-  //     },
-  //   ],
-  // });
-
-  // // Property Slide
-  // $(".property-slide").slick({
-  //   slidesToShow: 3,
-  //   arrows: false,
-  //   dots: true,
-  //   autoplay: true,
-  //   responsive: [
-  //     {
-  //       breakpoint: 1024,
-  //       settings: {
-  //         arrows: false,
-  //         slidesToShow: 2,
-  //       },
-  //     },
-  //     {
-  //       breakpoint: 600,
-  //       settings: {
-  //         arrows: false,
-  //         slidesToShow: 1,
-  //       },
-  //     },
-  //   ],
-  // });
-
-  // // location Slide
-  // $(".location-slide").slick({
-  //   slidesToShow: 4,
-  //   dots: true,
-  //   arrows: false,
-  //   autoplay: true,
-  //   responsive: [
-  //     {
-  //       breakpoint: 1024,
-  //       settings: {
-  //         arrows: false,
-  //         slidesToShow: 3,
-  //       },
-  //     },
-  //     {
-  //       breakpoint: 600,
-  //       settings: {
-  //         arrows: false,
-  //         slidesToShow: 1,
-  //       },
-  //     },
-  //   ],
-  // });
-
-  // // Team Slide
-  // $(".team-slide").slick({
-  //   slidesToShow: 4,
-  //   arrows: false,
-  //   autoplay: true,
-  //   dots: true,
-  //   responsive: [
-  //     {
-  //       breakpoint: 1023,
-  //       settings: {
-  //         arrows: false,
-  //         dots: true,
-  //         slidesToShow: 3,
-  //       },
-  //     },
-  //     {
-  //       breakpoint: 768,
-  //       settings: {
-  //         arrows: false,
-  //         slidesToShow: 2,
-  //       },
-  //     },
-  //     {
-  //       breakpoint: 480,
-  //       settings: {
-  //         arrows: false,
-  //         slidesToShow: 1,
-  //       },
-  //     },
-  //   ],
-  // });
-
-  // // Featured Slick Slider
-  // $(".featured_slick_gallery-slide").slick({
-  //   centerMode: true,
-  //   infinite: true,
-  //   centerPadding: "40px",
-  //   slidesToShow: 2,
-  //   responsive: [
-  //     {
-  //       breakpoint: 768,
-  //       settings: {
-  //         arrows: true,
-  //         centerMode: true,
-  //         centerPadding: "20px",
-  //         slidesToShow: 3,
-  //       },
-  //     },
-  //     {
-  //       breakpoint: 480,
-  //       settings: {
-  //         arrows: false,
-  //         centerMode: true,
-  //         centerPadding: "20px",
-  //         slidesToShow: 1,
-  //       },
-  //     },
-  //   ],
-  // });
-
-  // // Slider
-  // $(".clior").slick({
-  //   slidesToShow: 1,
-  //   slidesToScroll: 1,
-  //   arrows: false,
-  //   autoplay: true,
-  //   fade: true,
-  //   dots: true,
-  //   autoplaySpeed: 4000,
-  // });
-
-  // // Range Slider Script
-  // $(".js-range-slider").ionRangeSlider({
-  //   type: "double",
-  //   min: 0,
-  //   max: 1000,
-  //   from: 200,
-  //   to: 500,
-  //   grid: true,
-  // });
-
-  // // Select Bedrooms
-  // $("#bedrooms").select2({
-  //   placeholder: "Bedrooms",
-  //   allowClear: true,
-  // });
-
-  // // Select Bathrooms
-  // $("#bathrooms").select2({
-  //   placeholder: "Bathrooms",
-  //   allowClear: true,
-  // });
-
-  // // Select Property Types
-  // $("#ptypes").select2({
-  //   placeholder: "Mostrar tot",
-  //   allowClear: true,
-  // });
-
-  // // Select Property Types
-  // $("#status").select2({
-  //   placeholder: "Mostrar tot",
-  //   allowClear: true,
-  // });
-
-  // // specialisms
-  // $("#shorty").select2({
-  //   placeholder: "Mostrar tot",
-  //   allowClear: true,
-  // });
-
-  // // Select Min price
-  // $("#minprice").select2({
-  //   placeholder: "No Min",
-  //   allowClear: true,
-  // });
-
-  // // Select built
-  // $("#built").select2({
-  //   placeholder: "Year Built",
-  //   allowClear: true,
-  // });
-
-  // // Select Max Price
-  // $("#maxprice").select2({
-  //   placeholder: "No Max",
-  //   allowClear: true,
-  // });
-
-  // // Select Country
-  // $("#country").select2({
-  //   placeholder: "Country",
-  //   allowClear: true,
-  // });
-
-  // // Select Town
-  // $("#town").select2({
-  //   placeholder: "City/Town",
-  //   allowClear: true,
-  // });
-
-  // // Select Town
-  // $("#location").select2({
-  //   placeholder: "Shaow All",
-  //   allowClear: true,
-  // });
-
-  // // Select Cities
-  // $("#cities").select2({
-  //   placeholder: "All Cities",
-  //   allowClear: true,
-  // });
-
-  // // Select Status
-  // $("#status").select2({
-  //   placeholder: "Select Status",
-  //   allowClear: true,
-  // });
-
-  // // Select Rooms
-  // $("#rooms").select2({
-  //   placeholder: "Choose Rooms",
-  //   allowClear: true,
-  // });
-
-  // // Select Garage
-  // $("#garage").select2({
-  //   placeholder: "Choose Rooms",
-  //   allowClear: true,
-  // });
-
-  // // Select Rooms
-  // $("#bage").select2({
-  //   placeholder: "Select An Option",
-  //   allowClear: true,
-  // });
-
-  // // Home Slider
-  // $(".home-slider").slick({
-  //   centerMode: false,
-  //   slidesToShow: 1,
-  //   responsive: [
-  //     {
-  //       breakpoint: 768,
-  //       settings: {
-  //         arrows: true,
-  //         slidesToShow: 1,
-  //       },
-  //     },
-  //     {
-  //       breakpoint: 480,
-  //       settings: {
-  //         arrows: false,
-  //         slidesToShow: 1,
-  //       },
-  //     },
-  //   ],
-  // });
-
-  // $(".click").slick({
-  //   slidesToShow: 1,
-  //   slidesToScroll: 1,
-  //   autoplay: false,
-  //   autoplaySpeed: 2000,
-  // });
-
   // Advance Single Slider
   $(function () {
     // Card's slider
@@ -424,49 +77,6 @@ $(function () {
     });
   });
 
-  // // Featured Slick Slider
-  // $(".featured-slick-slide").slick({
-  //   centerMode: true,
-  //   centerPadding: "80px",
-  //   slidesToShow: 2,
-  //   responsive: [
-  //     {
-  //       breakpoint: 768,
-  //       settings: {
-  //         arrows: true,
-  //         centerMode: true,
-  //         centerPadding: "60px",
-  //         slidesToShow: 2,
-  //       },
-  //     },
-  //     {
-  //       breakpoint: 480,
-  //       settings: {
-  //         arrows: false,
-  //         centerMode: true,
-  //         centerPadding: "40px",
-  //         slidesToShow: 1,
-  //       },
-  //     },
-  //   ],
-  // });
-
-  // // MagnificPopup
-  // $("body").magnificPopup({
-  //   type: "image",
-  //   delegate: "a.mfp-gallery",
-  //   fixedContentPos: true,
-  //   fixedBgPos: true,
-  //   overflowY: "auto",
-  //   closeBtnInside: false,
-  //   preloader: true,
-  //   removalDelay: 0,
-  //   mainClass: "mfp-fade",
-  //   gallery: {
-  //     enabled: true,
-  //   },
-  // });
-
   // fullwidth home slider
   function inlineCSS() {
     $(".home-slider .item").each(function () {
@@ -483,33 +93,33 @@ $(function () {
   inlineCSS();
 
   // Search Radio
-  function searchTypeButtons() {
-    $('.property-search-type label.active input[type="radio"]').prop(
-      "checked",
-      true
-    );
-    var buttonWidth = $(".property-search-type label.active").width();
-    var arrowDist = $(".property-search-type label.active").position();
-    $(".property-search-type-arrow").css("left", arrowDist + buttonWidth / 2);
-    $(".property-search-type label").on("change", function () {
-      $('.property-search-type input[type="radio"]')
-        .parent("label")
-        .removeClass("active");
-      $('.property-search-type input[type="radio"]:checked')
-        .parent("label")
-        .addClass("active");
-      var buttonWidth = $(".property-search-type label.active").width();
-      var arrowDist = $(".property-search-type label.active").position().left;
-      $(".property-search-type-arrow").css({
-        left: arrowDist + buttonWidth / 1.7,
-        transition: "left 0.4s cubic-bezier(.95,-.41,.19,1.44)",
-      });
-    });
-  }
-  if ($(".hero-banner").length) {
-    searchTypeButtons();
-    $(window).on("load resize", function () {
-      searchTypeButtons();
-    });
-  }
+//   function searchTypeButtons() {
+//     $('.property-search-type label.active input[type="radio"]').prop(
+//       "checked",
+//       true
+//     );
+//     var buttonWidth = $(".property-search-type label.active").width();
+//     var arrowDist = $(".property-search-type label.active").position();
+//     $(".property-search-type-arrow").css("left", arrowDist + buttonWidth / 2);
+//     $(".property-search-type label").on("change", function () {
+//       $('.property-search-type input[type="radio"]')
+//         .parent("label")
+//         .removeClass("active");
+//       $('.property-search-type input[type="radio"]:checked')
+//         .parent("label")
+//         .addClass("active");
+//       var buttonWidth = $(".property-search-type label.active").width();
+//       var arrowDist = $(".property-search-type label.active").position().left;
+//       $(".property-search-type-arrow").css({
+//         left: arrowDist + buttonWidth / 1.7,
+//         transition: "left 0.4s cubic-bezier(.95,-.41,.19,1.44)",
+//       });
+//     });
+//   }
+//   if ($(".hero-banner").length) {
+//     searchTypeButtons();
+//     $(window).on("load resize", function () {
+//       searchTypeButtons();
+//     });
+//   }
 });
